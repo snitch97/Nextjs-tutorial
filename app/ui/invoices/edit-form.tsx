ca911e8f5836698d13617ca7bd32e42400ead5d2
snitch <suzukiiwao0820@gmail.com>
2024-08-26 18:23:56 +0900

    I have completed chpater 12. Mutatind data.

--- app/ui/invoices/edit-form.tsx
+++ app/ui/invoices/edit-form.tsx
@@ -9,6 +9,7 @@ import {
 } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 import { Button } from '@/app/ui/button';
+import { updateInvoice } from '@/app/lib/actions';
 
 export default function EditInvoiceForm({
   invoice,
@@ -17,10 +18,13 @@ export default function EditInvoiceForm({
   invoice: InvoiceForm;
   customers: CustomerField[];
 }) {
+  const updateInvoiceWithId = updateInvoice.bind(null, invoice.id);
   return (
-    <form>
+    <form action={updateInvoiceWithId}>
       <div className="rounded-md bg-gray-50 p-4 md:p-6">
+
         {/* Customer Name */}
+
         <div className="mb-4">
           <label htmlFor="customer" className="mb-2 block text-sm font-medium">
             Choose customer
@@ -46,6 +50,7 @@ export default function EditInvoiceForm({
         </div>
 
         {/* Invoice Amount */}
+
         <div className="mb-4">
           <label htmlFor="amount" className="mb-2 block text-sm font-medium">
             Choose an amount
@@ -67,6 +72,7 @@ export default function EditInvoiceForm({
         </div>
 
         {/* Invoice Status */}
+
         <fieldset>
           <legend className="mb-2 block text-sm font-medium">
             Set the invoice status
