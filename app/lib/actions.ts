71fc60fb9e85a33ebb79d2d0701a6267a6861af6
snitch <suzukiiwao0820@gmail.com>
2024-08-27 08:03:59 +0900

    I have completed chapter15. Adding Authentication

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,8 +1,11 @@
 "use server";
+
 import { z } from "zod";
 import { sql } from "@vercel/postgres"
 import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
+import { signIn } from "@/auth";
+import { AuthError } from "next-auth";
 
 const FormSchema = z.object({
   id: z.string(),
@@ -115,4 +118,20 @@ export async function deleteInvoce(id: string){
     };
   }
   // throw new Error("Failed");
+}
+
+export async function authenticate(prevState: string|undefined, formData:FormData){
+  try{
+    await signIn('credentials',formData);
+  }catch(error){
+    if(error instanceof AuthError){
+      switch(error.type){
+        case 'CredentialsSignin':
+          return 'Invalid credentials.';
+        default:
+          return 'Something went wrong.';
+      }
+    }
+    throw error;
+  }
 }
