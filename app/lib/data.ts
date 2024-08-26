de73492e4eba668c987e310923bf7def18725842
snitch <suzukiiwao0820@gmail.com>
2024-08-26 21:25:37 +0900

    I have completed chapter 13. Handling Errors

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -157,7 +157,6 @@ export async function fetchInvoiceById(id: string) {
       // Convert amount from cents to dollars
       amount: invoice.amount / 100,
     }));
-
     return invoice[0];
   } catch (error) {
     console.error('Database Error:', error);
